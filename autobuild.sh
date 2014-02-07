7bcf203b683e8dcc28d857131c926266021e4611
Jean-Baptiste Potonnier <JJeeb@users.noreply.github.com>
2014-02-07 17:50:12 +0100

    Create autobuild.sh

---
+++ autobuild.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+DIRECTORY_TO_OBSERVE="."
+BUILD_COMMAND="cabal build"
+EXCLUDE='\.git|\.idea|_bak___|\.swp|\.iml|\.#.*|.*flymake'
+
+function build {
+	bash -c "$BUILD_COMMAND"
+}
+
+function block_for_change {
+  inotifywait -r \
+			-e modify,move,create,delete \
+			--exclude $EXCLUDE \
+			$DIRECTORY_TO_OBSERVE 2>/dev/null
+}
+
+default='\e[39m'
+red='\e[31m'
+green='\e[32m'
+
+while block_for_change; do
+  clear
+  build
+  if [ $? -ne 0 ] 
+  then
+    echo -e "$red Failure $default"
+    notify-send "Build Failed" "$BUILD_COMMAND"
+  else
+    echo -e "$green Success $default"
+  fi	
+done
